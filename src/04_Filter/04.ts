3fe3b9a2885bfbf66127485bba287f1552f0869c
fedorgerasimov <1031577@mail.ru>
2022-03-18 11:39:33 +0300

    начало 04 видео - метод filter

---
+++ src/04_Filter/04.ts
@@ -0,0 +1,21 @@
+const ages = [18, 20, 22, 1, 100, 90 ,14]
+const predicate = (age:number) => {
+    return age > 90
+}
+
+type  CourseType = {
+    title: string
+    price: number
+}
+const courses = [
+    {title: 'CSS', price: 110},
+    {title: 'JS', price: 200},
+    {title: 'REACT', price: 150}
+]
+const cheapPredicate = (course: CourseType) => {
+    return course.price < 160
+}
+const chipCourse = [
+    {title: 'CSS', price: 110},
+    {title: 'REACT', price: 150}
+]
